7563797be29ad97292437e243e40704dc0ba8077
Louie Weng <louieweng@outlook.com>
2023-10-05 14:00:16 -0400

    chore: add dockerfile for backend (#16)
    add dockerfile for backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, Server};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello World" }));
+        
+    let addr = SocketAddr::from(([0, 0, 0, 0], 9000));
+
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
