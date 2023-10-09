cd3ed59cf6e7c96982e4fd0e1173bbdd307ddd78
Louie Weng <louieweng@outlook.com>
2023-10-09 09:19:33 -0400

    feat: add testing and project structure to backend (#21)
    * add postgres rust crate
    
    * add example handler with submodule system
    
    * add testing to main.rs
    
    * add backend tests to ci

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,45 @@
+use crate::handlers::hello_world::hello_world;
 use axum::{routing::get, Router, Server};
 use std::net::SocketAddr;
 
+mod handlers;
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(|| async { "Hello World" }));
-        
+    let app = app();
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 9000));
 
     Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+fn app() -> Router {
+    Router::new().route("/", get(hello_world))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::app;
+    use axum::body::Body;
+    use axum::http::{Request, StatusCode};
+    use tower::ServiceExt;
+
+    // reference from https://github.com/tokio-rs/axum/blob/main/examples/testing/src/main.rs
+    #[tokio::test]
+    async fn main() {
+        let app = app();
+
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"Hello World");
+    }
 }
